17a75e14be473d60385bdb3a102855f209bab3ba
KarH001 <karhunanyan@gmail.com>
2023-10-02 19:22:04 +0400

    Add New ScriptApp

---
+++ app.js
@@ -0,0 +1,50 @@
+// let num = 42
+// let firstName = 'Karen'
+// const isProgrammer = true
+// isProgrammer = folse // error
+
+//inchpes karox enq popoxakanner@ anvanel
+
+/* 
+let $ = 'test'
+let first_name = 'first' // bad
+myNum = 'name' // good
+let num42 = 35
+*/
+
+//inchpes chenq karox
+
+/* 
+let 42num = '11'
+let my-num = 11
+let nonst
+*/
+
+// inchpes nayel popoxakanum inch ka
+
+/*
+alert(firstName)
+console.log('test',firstname)
+*/
+
+// gortsoxutyunner popoxakannerov
+
+// console.log(num+10)
+// console.log(num-10)
+// console.log(num*10)
+// console.log(num/10)
+
+// let num2 = num + 10
+// console.log (num,num2)
+
+// num = num2 - num 
+
+// console.log(num,num2)
+
+// const fullName = firstName + ' ' + 'Hunanyan'
+// console.log(fullName)
+
+
+
+
+
